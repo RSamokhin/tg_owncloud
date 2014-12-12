203b462e436dc0762b31cba7eaa38d5ab7045c93
root <root@API-CLOUD-DEB>
2014-12-12 17:31:53 +0300

    initial clear release commit

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3b8268af19',
+  'passwordsalt' => 'dc8c8ecefcaa7da20d4ffacf812519',
+  'secret' => 'a567d8c27c187c8435a93969f8451e3d028e85f5835b0100e968e6f815bb0ce33bb005f9840b21e445e065f1585be2a3',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.41',
+  ),
+  'datadirectory' => '/var/www/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.1.41/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '7.0.4.2',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'owncloud',
+  'dbpassword' => 'Warpten13',
+  'installed' => true,
+);
