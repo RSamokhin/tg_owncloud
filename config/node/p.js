c8fe2860c81a75340bd9048c88d1f0c72bd7d73a
root <root@API-CLOUD-DEB>
2014-12-17 01:34:09 +0300

    argsv added + check email commit

--- config/node/p.js
+++ config/node/p.js
@@ -1,37 +1,54 @@
-var request = require('request');
+var usr = process.argv[2];
+var pwd = process.argv[3];
+var chk = process.argv[4];
 
-var j = request.jar();
-var request = request.defaults({jar:j});
-
-request('http://192.168.1.41/owncloud/index.php', function (error, response, body) {
-	if (error && response.statusCode !== 200)
-		return;
-		
-	var token = /data-requesttoken="([^"]+)"/.exec(body)[1];
-	
-	request.post({url:'http://192.168.1.41/owncloud/index.php', form: {
-		'user': 'rsamokhin',
-		'password': 'Qsx159357',
-		'timezone-offset': 3,
-		'requesttoken': token
-	}}, function(err, httpResponse, body){
-
-		request('http://192.168.1.41/owncloud/index.php/settings/users', function (error, response, body) {
+var fdata = Math.random();
+fs = require('fs')
+fs.readFile('config/node/tmpdir/'+usr, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+	fdata=data.trim();
+	if (fdata==chk){
+		var request = require('request');
+		var j = request.jar();
+		var request = request.defaults({jar:j});
+		request('http://localhost/owncloud/index.php', function (error, response, body) {
+			if (error && response.statusCode !== 200)
+				return;
 			var token = /data-requesttoken="([^"]+)"/.exec(body)[1];
-			
-			request.post({
-				url:'http://192.168.1.41/owncloud/index.php/settings/ajax/createuser.php', 
-				form: {
-					'username': 'test' + Math.random(),
-					'password': 'test',
-					'groups': ''
-				},
-				headers: {
-					'requesttoken': token
-				}
-			}, function(err, httpResponse, body){
-				console.log(body);
+			request.post({url:'http://localhost/owncloud/index.php', form: {
+				'user': 'rsamokhin',
+				'password': 'Qsx159357',
+				'timezone-offset': 3,
+				'requesttoken': token
+			}}, function(err, httpResponse, body){
+				request('http://localhost/owncloud/index.php/settings/users', function (error, response, body) {
+					var token = /data-requesttoken="([^"]+)"/.exec(body)[1];
+					request.post({
+						url:'http://localhost/owncloud/index.php/settings/ajax/createuser.php', 
+						form: {
+							'username': usr,
+							'password': pwd,
+							'groups': ''
+						},
+						headers: {
+							'requesttoken': token
+						}
+					}, function(err, httpResponse, body){
+						console.log(body);
+					});
+				});
 			});
 		});
-	});
+	}else{
+		var rdata = {
+						"data":{
+								"message":"Некорректный код подтверждения, запросите код повторно"
+						},
+						"status":"error"
+					}
+		console.log(rdata.data)
+	}
 });
+
