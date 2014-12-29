b08317d8d69f3481b491d680c99a69d11dc5e83b
root <poshliemail@googlemail.com>
2014-12-29 20:41:49 +0300

    checks for length and email regexp added. clickes binded

--- core/js/srg.js
+++ core/js/srg.js
@@ -2,7 +2,10 @@ $(function(){
     registerFormDiv = $('<div/>').addClass('registerFormDiv').attr({
         'title':'Register new user'
     });
-    validateTips = $('<p/>').addClass('validateTips').html('All form fields are required.');
+    validateTips = $('<p/>').addClass('validateTips').html('All form fields are required.').css({
+         'border': '1px solid transparent',
+         'padding': '0.3em' 
+    });
     registerForm = $('<div/>').addClass('registerForm');
     registerFormFieldSet = $('<fieldset/>').addClass('registerFormFieldSet').css({
         'display':'inline-block',
@@ -17,7 +20,7 @@ $(function(){
     });
     registerFoormEmailInput = $('<input/>').addClass('registerFoormEmailInput text ui-widget-content ui-corner-all').attr({
         'type':'text',
-        'name':'email'
+        'name':'Email'
     }).css({
        'float': 'right' 
     });
@@ -31,7 +34,8 @@ $(function(){
     });
     registerFoormTokenInput = $('<input/>').addClass('registerFoormTokenInput text ui-widget-content ui-corner-all').attr({
         'type':'text',
-        'name':'token'
+        'name':'Token',
+        'disabled':'disabled'
     }).css({
        'float': 'right' 
     });
@@ -44,7 +48,8 @@ $(function(){
     });
     registerFoormPwdInput = $('<input/>').addClass('registerFoormPwdInput text ui-widget-content ui-corner-all').attr({
         'type':'password',
-        'name':'password'
+        'name':'Password',
+        'disabled':'disabled'
     }).css({
        'float': 'right' 
     });;
@@ -65,7 +70,7 @@ $(function(){
     
     $('.register-button').parent().bind('click',function(){
         registerFormDiv.dialog({
-            height: 330,
+            height: 340,
             width: 400,
             modal: true,
             resizable:false,
@@ -76,8 +81,63 @@ $(function(){
               }
             }
         });
+        $(".ui-dialog-buttonpane button").eq(0).button("disable").removeClass("ui-state-default");
     });
+    registerFoormRequestTokenButton.bind('click',function(){
+        email = $( ".registerFoormEmailInput" );
+        allFields = $( [] ).add( email );
+        tips = $( ".validateTips" );
+        var valid = true;
+        valid = valid && checkRegexp( email, emailRegex, "eg. ui@email.com" );
+        if ( valid ) {
+            allFields.removeClass( "ui-state-error" );
+        }
+        return valid
+    });
+    emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 });
 function register(){
-    alert(this);
+    token = $( ".registerFoormTokenInput" );
+    email = $( ".registerFoormEmailInput" );
+    password = $( ".registerFoormPwdInput" );
+    allFields = $( [] ).add( token ).add( email ).add( password );
+    tips = $( ".validateTips" );
+    var valid = true;
+    allFields.removeClass( "ui-state-error" );
+    valid = valid && checkLength( token, "Token", 5, 20 );
+    valid = valid && checkLength( password, "Password", 5, 16 );
+    valid = valid && checkRegexp( token, /^[a-z]([0-9a-z_\s])+$/i, "Token must be requested and sent to your email" );
+    valid = valid && checkRegexp( email, emailRegex, "eg. ui@email.com" );
+    valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+    if ( valid ) {
+        alert(this);
+    }
+    return valid;
 } 
+function updateTips( t ) {
+      tips
+        .text( t )
+        .addClass( "ui-state-highlight" );
+      setTimeout(function() {
+        tips.removeClass( "ui-state-highlight", 1500 );
+      }, 500 );
+}
+function checkLength( o, n, min, max ) {
+      if ( o.val().length > max || o.val().length < min ) {
+        o.addClass( "ui-state-error" );
+        updateTips( "Length of " + n + " must be between " +
+          min + " and " + max + "." );
+        return false;
+      } else {
+        return true;
+      }
+}
+function checkRegexp( o, regexp, n ) {
+      if ( !( regexp.test( o.val() ) ) ) {
+        o.addClass( "ui-state-error" );
+        updateTips( n );
+        return false;
+      } else {
+        return true;
+      }
+}
