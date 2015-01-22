d44fcafb01afe1559ea9e2969f60e14b98494c82
rsamokhin <poshliemail@googlemail.com>
2015-01-22 13:10:04 +0300

    clear and combine the new form function

--- core/js/srg.js
+++ core/js/srg.js
@@ -1,72 +1,74 @@
 $(function(){
-    registerFormDiv = $('<div/>').addClass('registerFormDiv');
-    validateTips = $('<p/>').addClass('validateTips').html('All form fields are required.').css({
-         'border': '1px solid transparent',
-         'padding': '0.3em' 
-    });
-    registerForm = $('<div/>').addClass('registerForm');
-    registerFormFieldSet = $('<fieldset/>').addClass('registerFormFieldSet').css({
-        'display':'inline-block',
-        'min-width': '100px'
-    });
-    registerFoormEmailP =$('<p/>').addClass('registerFoormEmailP registerFormP');
-    registerFoormEmailLabel = $('<label/>').addClass('registerFoormEmailLabel').attr({
-        'for':'email'
-    }).html('Email').css({
-        'position': 'relative',
-        'top': '17px'
-    });
-    registerFoormEmailInput = $('<input/>').addClass('registerFoormEmailInput text ui-widget-content ui-corner-all').attr({
-        'type':'text',
-        'name':'Email'
-    }).css({
-       'float': 'right' 
-    });
-    registerFoormRequestTokenButton = $('<button/>').html('Request token');
-    registerFoormTokenP =$('<p/>').addClass('registerFoormTokenP registerFormP');
-    registerFoormTokenLabel = $('<label/>').addClass('registerFoormTokenLabel').attr({
-        'for':'Token'
-    }).html('Token').css({
-        'position': 'relative',
-        'top': '15px'
-    });
-    registerFoormTokenInput = $('<input/>').addClass('registerFoormTokenInput text ui-widget-content ui-corner-all').attr({
-        'type':'text',
-        'name':'Token',
-        'disabled':'disabled'
-    }).css({
-       'float': 'right' 
-    });
-    registerFoormPwdP =$('<p/>').addClass('registerFoormPwdP registerFormP');
-    registerFoormPwdLabel = $('<label/>').addClass('registerFoormPwdLabel').attr({
-        'for':'Password'
-    }).html('Password').css({
-        'position': 'relative',
-        'top': '52px'
-    });
-    registerFoormPwdInput = $('<input/>').addClass('registerFoormPwdInput text ui-widget-content ui-corner-all').attr({
-        'type':'password',
-        'name':'Password',
-        'disabled':'disabled'
-    }).css({
-       'float': 'right' 
-    });;
-    
-    validateTips.appendTo(registerFormDiv);
-    registerForm.appendTo(registerFormDiv);
-    registerFormFieldSet.appendTo(registerForm);
-    registerFoormEmailP.appendTo(registerFormFieldSet);
-    registerFoormEmailLabel.appendTo(registerFoormEmailP);
-    registerFoormEmailInput.appendTo(registerFoormEmailP);
-    registerFoormRequestTokenButton.appendTo(registerFoormEmailP);
-    registerFoormTokenP.appendTo(registerFormFieldSet);
-    registerFoormTokenLabel.appendTo(registerFoormTokenP);
-    registerFoormTokenInput.appendTo(registerFoormTokenP);
-    registerFoormPwdP.appendTo(registerFormFieldSet);
-    registerFoormPwdLabel.appendTo(registerFoormPwdP);
-    registerFoormPwdInput.appendTo(registerFoormPwdP);
-    
+    function combineTheForm(){
+        registerFormDiv = $('<div/>').addClass('registerFormDiv');
+        validateTips = $('<p/>').addClass('validateTips').html('All form fields are required.').css({
+            'border': '1px solid transparent',
+            'padding': '0.3em'
+        });
+        registerForm = $('<div/>').addClass('registerForm');
+        registerFormFieldSet = $('<fieldset/>').addClass('registerFormFieldSet').css({
+            'display':'inline-block',
+            'min-width': '100px'
+        });
+        registerFoormEmailP =$('<p/>').addClass('registerFoormEmailP registerFormP');
+        registerFoormEmailLabel = $('<label/>').addClass('registerFoormEmailLabel').attr({
+            'for':'email'
+        }).html('Email').css({
+            'position': 'relative',
+            'top': '17px'
+        });
+        registerFoormEmailInput = $('<input/>').addClass('registerFoormEmailInput text ui-widget-content ui-corner-all').attr({
+            'type':'text',
+            'name':'Email'
+        }).css({
+            'float': 'right'
+        });
+        registerFoormRequestTokenButton = $('<button/>').html('Request token');
+        registerFoormTokenP =$('<p/>').addClass('registerFoormTokenP registerFormP');
+        registerFoormTokenLabel = $('<label/>').addClass('registerFoormTokenLabel').attr({
+            'for':'Token'
+        }).html('Token').css({
+            'position': 'relative',
+            'top': '15px'
+        });
+        registerFoormTokenInput = $('<input/>').addClass('registerFoormTokenInput text ui-widget-content ui-corner-all').attr({
+            'type':'text',
+            'name':'Token',
+            'disabled':'disabled'
+        }).css({
+            'float': 'right'
+        });
+        registerFoormPwdP =$('<p/>').addClass('registerFoormPwdP registerFormP');
+        registerFoormPwdLabel = $('<label/>').addClass('registerFoormPwdLabel').attr({
+            'for':'Password'
+        }).html('Password').css({
+            'position': 'relative',
+            'top': '52px'
+        });
+        registerFoormPwdInput = $('<input/>').addClass('registerFoormPwdInput text ui-widget-content ui-corner-all').attr({
+            'type':'password',
+            'name':'Password',
+            'disabled':'disabled'
+        }).css({
+            'float': 'right'
+        });;
+        validateTips.appendTo(registerFormDiv);
+        registerForm.appendTo(registerFormDiv);
+        registerFormFieldSet.appendTo(registerForm);
+        registerFoormEmailP.appendTo(registerFormFieldSet);
+        registerFoormEmailLabel.appendTo(registerFoormEmailP);
+        registerFoormEmailInput.appendTo(registerFoormEmailP);
+        registerFoormRequestTokenButton.appendTo(registerFoormEmailP);
+        registerFoormTokenP.appendTo(registerFormFieldSet);
+        registerFoormTokenLabel.appendTo(registerFoormTokenP);
+        registerFoormTokenInput.appendTo(registerFoormTokenP);
+        registerFoormPwdP.appendTo(registerFormFieldSet);
+        registerFoormPwdLabel.appendTo(registerFoormPwdP);
+        registerFoormPwdInput.appendTo(registerFoormPwdP);
+    }
     $('.recoverPassword').bind('click',function(){
+        $('.registerFormDiv').remove();
+        combineTheForm();
         registerFormDiv.attr({
             'title':'Recover the password'
         });
@@ -87,6 +89,8 @@ $(function(){
     
     
     $('.register-button').parent().bind('click',function(){
+        $('.registerFormDiv').remove();
+        combineTheForm();
         registerFormDiv.attr({
             'title':'Register new user'
         });
